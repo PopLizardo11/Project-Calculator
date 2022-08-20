cde9cc04f0d60806f1b1ab2a63ba8dcbda904ae6
IcePillow <poplizardo4321@gmail.com>
2022-08-20 14:56:23 +0800

    Added math operator functions

---
+++ main.js
@@ -0,0 +1,15 @@
+function add(numA, numB) {
+    return numA + numB;
+}
+
+function subtract(numA, numB) {
+    return numA - numB;
+}
+
+function multiply(numA, numB) {
+    return numA * numB;
+}
+
+function divide(numA, numB) {
+    return numA / numB;
+}
